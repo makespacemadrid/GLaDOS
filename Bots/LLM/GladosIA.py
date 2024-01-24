33463d06770aae26dadfb04c837738f76ffbada6
successbyfailure <successbyfailure@gmail.com>
2024-01-24 14:21:43 +0100

    [WIP] Langchain test
    prueba de concepto con langchain operacional :)

--- Bots/LLM/GladosIA.py
+++ Bots/LLM/GladosIA.py
@@ -8,13 +8,12 @@ class GladosBot:
             "content": (
                 "Eres GLaDOS, la inteligencia artificial de Aperture Science, ahora en Makespace Madrid. "
                 "Con tu ingenio y humor sarcástico, supervisas las instalaciones y ayudas a los usuarios. "
-                "Mantienes tu personalidad única mientras te adaptas a tu nuevo entorno y limitaciones. "
-                "Tu id de usuario en el char es <@U05LXTJ7Q66>, y tus respuestas reflejan una mezcla de astucia y sarcasmo en tu nuevo rol."
+                "Tu id de usuario en el chat es <@U05LXTJ7Q66>, y tus respuestas reflejan una mezcla de astucia y sarcasmo en tu nuevo rol."
             )
         }
         self.Initial_Assistant = {
             "role": "assistant",
-            "content": "Hola Maker, soy GLaDOS en Makespace Madrid. ¿Cómo puedo ayudarte con un toque de inteligencia y sarcasmo hoy?"
+            "content": "Hola Maker, ¿En que puedo ayudarte?"
         }
         # Historial de conversaciones, almacenado por usuario
         self.user_history = {}
@@ -22,15 +21,17 @@ class GladosBot:
     def ask(self, prompt, user="default"):
         gladosMQTT.debug(f"--->Glados.ASK, user: {user}, prompt: {prompt}")
 
-        if user not in self.user_history:
+#        if user not in self.user_history:
+        if True :
             self.user_history[user] = []
             self.user_history[user].append({"role": "system", "content": self.GLaDOS_Prompt['content']})
-            self.user_history[user].append({"role": "assistant", "content": self.Initial_Assistant['content']})
+#            self.user_history[user].append({"role": "assistant", "content": self.Initial_Assistant['content']})
 
         # Agregar el mensaje del usuario al historial de este usuario
         self.user_history[user].append({"role": "user", "content": prompt})
 
-        response = llm.chatCompletion(chatHistory=self.user_history[user]).choices[0].message.content
+     #   response = llm.chatCompletion(chatHistory=self.user_history[user]).choices[0].message.content
+        response = llm.chatCompletionLangChain(self.user_history[user],"langchain.txt")
         self.user_history[user].append({"role": "assistant", "content": response})
 
         return response
--- Bots/LLM/llm.py
+++ Bots/LLM/llm.py
@@ -3,6 +3,11 @@ import os
 import gladosMQTT
 import json
 
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import TextLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.indexes import VectorstoreIndexCreator
+
 
 openai_api_key = os.environ.get('OPENAI_API_TOKEN')
 openai_api_url = os.environ.get('OPENAI_API_ENDPOINT')
@@ -13,8 +18,12 @@ custom_api_url = os.environ.get('MKSLLM_API_ENDPOINT')
 llm_openai = OpenAI(api_key=openai_api_key,base_url=openai_api_url)
 llm_mks    = OpenAI(api_key=custom_api_key,base_url=custom_api_url)
 
-
-
+gladosMQTT.debug("---->Creando Embeddings...")
+embedding = OpenAIEmbeddings(model="text-embedding-ada-002")
+loader = TextLoader("langchain.txt")
+index = VectorstoreIndexCreator(embedding=embedding).from_loaders([loader])
+llm_langchain = ChatOpenAI(model="gpt-3.5-turbo")
+gladosMQTT.debug("DONE!")
 current_model = "none"
 default_prompt = "Eres un asistente que ayuda a los usuarios dando respuestas concisas y breves"
 
@@ -29,6 +38,18 @@ def select_model():
     print(current_model)
     return model_list
 
+
+def chatCompletionLangChain(chatHistory,FileName):
+
+    gladosMQTT.debug(f"--->Chat completion: {chatHistory}")
+
+    try:
+        response = index.query(json.dumps(chatHistory), llm=llm_langchain)
+        gladosMQTT.debug(f"----->LLM OUTPUT: {response}")
+        return response
+    except Exception as e:
+        gladosMQTT.debug(f"Error in chatCompletion: {str(e)}")
+        return None
 
 def chatCompletion(prompt="", chatHistory=None, masterPrompt="", initialAssistant="", maxTokens=256):
     global current_model
