6074fcb2f36826af22a97487b270a7d1a6dbafbc
successbyfailure <successbyfailure@gmail.com>
2024-01-29 17:10:31 +0100

    WIP telegram node
    Revision inicial, sigo batallando el tema de que aveces el .env no se carga y otras veces se carga pero a la libreria mqtt no le gusta el nombre del host wtf

--- Bots/Telegram/node.py
+++ Bots/Telegram/node.py
@@ -5,29 +5,33 @@ import gladosMQTT
 import platform
 from telethon import TelegramClient, events
 
+#ENV
+from dotenv import load_dotenv
+load_dotenv()
+
 # Configuración de Telegram
 api_id = os.environ.get("TELEGRAM_API_ID")  # Reemplaza con tu propio api_id
 api_hash = os.environ.get("TELEGRAM_API_HASH")  # Reemplaza con tu propio api_hash
 telegram_token = os.environ.get("TELEGRAM_BOT_TOKEN")  # Reemplaza con tu token de bot
 
 # Configuración MQTT
-mqHost	 = os.environ.get("MQTT_HOST")
-mqPort 	 = os.environ.get("MQTT_PORT")
+#Variables
+mqHost	 = os.environ.get("MQTT_HOST", "mqtt.makespacemadrid.org") #WTF! No entiendo que pasa con las variables de entorno que vienen del compose :S
+mqPort 	 = os.environ.get("MQTT_PORT", 1883)
 nodeName = platform.node()
 
-# Iniciar cliente de Telegram
-telegram_client = TelegramClient('telegram_bot_session', api_id, api_hash)
-
-# Conectar con MQTT
-gladosMQTT.initMQTT(mqHost, mqPort, nodeName, lambda *args: None, lambda *args: None, lambda *args: None)
 
 # Temas MQTT para comunicarse con otros componentes
 topic_telegram = "comms/telegram"
-topic_glados_send_msg = topic_telegram + "/send"
+topic_telegram_send_msg = topic_telegram + "/send"
+topic_telegram_event = topic_telegram + "/event"
 
+
+# Iniciar cliente de Telegram
+telegram_client = TelegramClient('/tmp/telegram_bot_session', api_id, api_hash)
 
 def subscribeTopics() :
-	gladosMQTT.subscribe(topic_glados_send_msg)
+	gladosMQTT.subscribe(topic_telegram_send_msg)
 
 
 def on_connect(client, userdata, rc,arg):
@@ -37,7 +41,7 @@ def on_disconnect(client, userdata, rc):
 	gladosMQTT.debug("Disconnected! rc: "+str(rc))
 
 def on_message(client, userdata, msg):
-	if (msg.topic == topic_glados_send_msg) :
+	if (msg.topic == topic_telegram_send_msg) :
 		try:
 			payload = msg.payload.decode('utf-8')
 			data = json.loads(payload)
@@ -48,19 +52,17 @@ def on_message(client, userdata, msg):
 			gladosMQTT.debug("Error al parsear JSON:")
       
 # Enviar mensajes a Telegram
-def sendTelegramMsg(id, msg):
-    response = json.dumps({"dest": id, "msg": msg})
-    gladosMQTT.debug(f"--->Respuesta a Telegram: {response}")
-    gladosMQTT.publish(topic_glados_send_msg, response)
+async def sendTelegramMsg(user_id, msg):
+    gladosMQTT.debug(f"--->Respuesta a Telegram: {user_id} : {msg}")
+    await telegram_client.send_message(user_id, msg)    
 
 # Evento para manejar nuevos mensajes en Telegram
 @telegram_client.on(events.NewMessage)
 async def handle_new_message(event):
+    gladosMQTT.publish(topic_telegram_event, json.dumps(event)) 
     if event.is_private:
         msg = event.message.message
         chat_id = event.message.chat_id
-        # Aquí puedes añadir lógica adicional para procesar el mensaje
-        # Por ejemplo, enviarlo a otro servicio o responder directamente
         await event.respond('Recibí tu mensaje: ' + msg)  # Respuesta de ejemplo
 
 # Iniciar el bot de Telegram
@@ -68,5 +70,8 @@ def start_telegram_bot():
     with telegram_client:
         telegram_client.run_until_disconnected()
 
+
+
 if __name__ == "__main__":
+    gladosMQTT.initMQTT(mqHost,mqPort,nodeName,on_connect,on_message,on_disconnect)
     start_telegram_bot()
